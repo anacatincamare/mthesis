939fae4149c57402773f07896e736e4a62294596
Ana-Catinca Mare <ana.mare@student.auc.nl>
2026-09-25 13:41:37 +0200

    added zn squared statistic function according to Buccheri et al. 1983

---
+++ zn_squared.py
@@ -0,0 +1,13 @@
+
+def zn_squared(phase_model, n):
+    """
+    Function takes the phase model and the number of harmonics (n) and returns the Zn-squared value of that phase model.
+
+    Takes: 
+        phase_model: array of times of arrivals converted into phase model.
+        n: int, number of harmonics
+    """
+    N = len(toas_d) # total number of detections
+    phases = np.outer(np.arange(1, n+1), toas_d)
+    zn_sq = (2.0 / N) * np.sum(np.sum(np.cos(phases), axis = 1) ** 2 + np.sum(np.sin(phases), axis = 1) ** 2)
+    return zn_sq
